db04727e84df5e7d7d70f60bcaf4cc9881ceca83
Gru <78332542+gru2007@users.noreply.github.com>
2025-02-12 08:01:24 +1000

    Update common-constants.ts

--- src/constants/common-constants.ts
+++ src/constants/common-constants.ts
@@ -1,4 +1,4 @@
 import { Address } from '@ton/core';
 
-export const USDT_MASTER_ADDRESS = Address.parse('kQD0GKBM8ZbryVk2aESmzfU6b9b_8era_IkvBSELujFZPsyy');
-export const INVOICE_WALLET_ADDRESS = Address.parse('0QD-SuoCHsCL2pIZfE8IAKsjc0aDpDUQAoo-ALHl2mje04A-');
+export const USDT_MASTER_ADDRESS = Address.parse('kQBJdhWTzvLT90QnChNarjXLvX4vDIGl433TFSJf4a7w4onW');
+export const INVOICE_WALLET_ADDRESS = Address.parse('kQDJM53nx9FOeVkva8mjp-oti03QdZcKi5qdQj4yhet19UzN');
